63300a6762c6e2e0e294b33f3d22ee8db222e177
Weixiong Lin <wx_lin@sjtu.edu.cn>
2021-05-25 14:04:10 +0800

    update connection.html

--- modules/scholar/views.py
+++ modules/scholar/views.py
@@ -254,13 +254,9 @@ def connection(name="Quanshi Zhang"):
                         "Awards": json.loads(researcher.Awards)["Awards"],
                         "Co_authors": json.loads(researcher.Co_authors)["Co_authors"],
                         "Papers": json.loads(researcher.Papers)["Papers"],
-                        # "Cited_graph": json.loads(researcher.Cited_graph)["Cited_graph"],
+                        "Cited_graph": json.loads(researcher.Cited_graph)["Cited_graph"],
                     }
-    # print("Researcher_info[Papers]", researcher_info["Papers"])
-    citation = researcher.Cited_graph
-    print("Citation", citation)
-    # return render_template("search/connection.html", Researcher_info=researcher_info, Citation=json.dumps(json.loads(researcher.Cited_graph)))
-    return render_template("search/connection.html", Researcher_info=researcher_info, Citation=citation)
+    return render_template("search/connection.html", Researcher_info=researcher_info)
 
 
 @scholar_blue.route("/operateFavor", methods=["POST"])
